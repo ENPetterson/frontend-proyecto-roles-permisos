53ef17dfbe362a564bb34df3ba9c69a20338d75c
Erick Petterson <e.n.petterson@gmail.com>
2024-03-03 18:01:15 -0300

    Vue Login

---
+++ src/service/Api.js
@@ -0,0 +1,38 @@
+import axios from "axios"
+// Esto conecta con endpoint de backend
+export const urlBase = "http://127.0.0.1:8000/api";
+
+const App = () => {
+    let token = "";
+
+    const api = axios.create({
+        baseURL: urlBase,
+        headers: {
+            Accept: 'application/json',
+            Authorization: 'Bearer '+token
+        }
+    });
+
+    //capturar errores
+    api.interceptors.response.use(
+        (response) => {
+            return response;
+        },
+        (error) => {
+            if(error.response.status === 401){
+
+            }
+            if(error.response.status === 403){
+
+            }
+
+            return Promise.reject(error);
+
+        }
+        
+    )
+    return api;
+
+}
+
+export default App;
